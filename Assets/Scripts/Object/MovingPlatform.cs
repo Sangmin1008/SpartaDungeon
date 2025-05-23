a37f12fedbadfa7471c8da57693ec237d6c634d5
Sangmin1008 <dkdhakzm@gmail.com>
2025-05-23 10:50:49 +0900

    feat : 움직이는 발판 구현

--- Assets/Scripts/Object/MovingPlatform.cs
+++ Assets/Scripts/Object/MovingPlatform.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float waitTimeAtPoint = 1f;
+
+    private int _currentTargetIndex = 0;
+    private float _waitTimer = 0f;
+
+    private void Start()
     {
-        
+        if (waypoints.Length < 2)
+            Debug.LogWarning("이동 장소 2개 미만");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (waypoints.Length < 2) return;
+        MovePlatform();
+    }
+
+    private void MovePlatform()
+    {
+        Vector3 targetPos = waypoints[_currentTargetIndex].position;
+        Vector3 currentPos = transform.position;
+
+        if (Vector3.Distance(currentPos, targetPos) < 0.05f)
+        {
+            _waitTimer += Time.fixedDeltaTime;
+            if (_waitTimer >= waitTimeAtPoint)
+            {
+                _waitTimer = 0f;
+                _currentTargetIndex = (_currentTargetIndex + 1) % waypoints.Length;
+            }
+        }
+        else
+        {
+            Vector3 direction = (targetPos - currentPos).normalized;
+            transform.position += direction * moveSpeed * Time.fixedDeltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
     }
 }
